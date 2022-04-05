fbe38aa4021a64f7f76c9999f2fb6213df72c037
annie-2686 <97668438+annie-2686@users.noreply.github.com>
2022-04-05 14:40:12 +0100

    added dictionary, meaning, result components

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results.js";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
   function handleResponse(response) {
     console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
   }
-
   let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
   axios.get(apiURL).then(handleResponse);
-
+  
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
@@ -23,6 +25,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
